b22cd7b4c07421a154379e86dfa0d4385bce251c
skyboybb <2488445092@qq.com>
2024-12-15 17:26:16 +0800

    Changes

---
+++ utils/datasets/__init__.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+sys.path.append('D:/pycharm/pythonProject/ms_CNN/utils/datasets')
+from CWRUPath_sets import CWRUPath_sets
+from CWRUwpd_sets import CWRUwpd_sets
+from CWRUms_sets import CWRUms_sets
+from CWRUPathMSFFT_sets import CWRUPathMSFFT_sets
+from CWRUTD_sets import CWRUTD_sets
+from CWRUTDPath_sets import CWRUTDPath_sets
+from CWRUTDPathmethod2 import CWRUTDPathmethod2
+
+
+from MotorPath_sets import MotorPath_sets
+from Motorwpd_sets import Motorwpd_sets
+from Motorms_sets import Motorms_sets
+from MotorMSFFT_sets import MotorMSFFT_sets
+from MotorPathMSFFT_sets import MotorPathMSFFT_sets
+from MotorTD_sets import MotorTD_sets
+from MotorPathmethod2 import MotorPathmethod2
+
+
+from PDBONms_sets import PDBONms_sets
+from PDBONwpd_sets import PDBONwpd_sets
+from PDBONPath_sets import PDBONPath_sets
+from PDBONMSFFT_sets_ import PDBONMSFFT_sets
+from PDBONPathMSFFT_sets import PDBONPathMSFFT_sets
+from PDBONTD_sets  import PDBONTD_sets
+from PDBONPathmethod2 import PDBONPathmethod2
+
+
+
+from CUms_sets import CUms_sets
+from CUwpd_sets import CUwpd_sets
+from CUPath_sets import CUPath_sets
+from CUMSFFT_sets import CUMSFFT_sets
+from CUPathMSFFT_sets import CUPathMSFFT_sets
+from CUTDorFFT_sets import CUTDorFFT_sets
+from CUPathmethod2 import CUPathmethod2
+
+
+from Gearms_sets import Gearms_sets
+from Gearwpd_sets import Gearwpd_sets
+from GearPath_sets import GearPath_sets
+from GearPathMSFFT_sets import GearPathMSFFT_sets
+from GearTDorFFT_sets import GearTDorFFT_sets
+from GearPathmethod2 import GearPathmethod2
+
+
+
+
---
+++ utils/models/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+sys.path.append('D:/pycharm/pythonProject/ms_CNN/utils/models')
+
+from ChebyNet import ChebyNet
+from WPDChebyNet_3 import ChebyNet_3
+from WPDChebyNetKAN_3 import ChebyNetKAN_3
+
+from WPDCNN_3KAN import WPD_CNN_3KAN
+
+
+from MSCNN_3KAN import MSCNN_3KAN
+from MMM_FFTCNN import MMM_FFTCNN
+from MMMChebyNetKAN import MMMChebyNetKAN
+from MMMChebyNetKAN_method2 import  MMMChebyNetKAN_method2
+
+from MLP import MLP
+from FKAN import FKAN
+from CNN import CNN
+from WPDCNN_3 import WPD_CNN_3
+from MSCNN_3 import MSCNN_3
+from MC_CNN import MC_CNN
+from LSTM import LSTMModel
+from ResNet import ResNet1D
